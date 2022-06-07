79fb95da08aa1eadaae3ddffcc16955d088d7021
Elvis-Guy <guyelvisbakunzi@gmail.com>
2022-06-07 00:07:15 -0700

    changes

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,14 @@
-#include "School.h"
+#include "stdio.h"
 /**
- * main - prints the string "School" from a character array.
+ * main - prints the string "_putchar" from a character array.
  * Return: 0 on success.
  */
 int main(void)
 {
-	char c[] = "School";
+	char c[] = "_putchar";
 	int i;
 
-	for (i = 0; i < 6; i++)
+	for (i = 0; i < 8; i++)
 	{
 		_putchar(c[i]);
 	}
